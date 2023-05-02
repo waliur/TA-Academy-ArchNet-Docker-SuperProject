92bee8c4cb9087042f1d527c346918fadb261d70
Waliur Rahman <maltenrazer@gmail.com>
2023-05-02 10:14:09 +0100

    WR: Setup pgadmin

--- docker/node/node1/server.js
+++ docker/node/node1/server.js
@@ -9,9 +9,12 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get(['/', '/CS1'], (req, res) => {
-  res.send('Hello World - NodeJS - CS1');
+  console.warn(req);
+  console.warn('Inside App.get');
+  res.send('Hello World - NodeJS - CS1....');
+  console.log('After render');
 });
 
 app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}:${PORT}`);
+  console.log(`Running on http://${HOST}:${PORT} app.listen`);
 });
